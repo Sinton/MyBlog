489f92e086ce9d3358994340d29bed5496f6a330
Sinton <yansuntong@gmail.com>
2026-06-29 20:12:54 +0800

    feat: update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
+  site: "https://example.com", // 替换为你的真实博客域名，用于生成 Canonical URL 和站点地图
   integrations: [mdx()],
 });
